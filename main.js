6821fc3615068f4872c182581a3991f99bdc7b6f
oOzhuoxiaOo <1851428933@qq.com>
2023-03-19 17:00:45 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 import App from './App'
 
+// 导入网络请求的包
+import {$http} from "@escook/request-miniprogram"
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = "https://www.uinav.com"
+
+// 请求拦截器(路由守卫?)
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:"数据加载中"
+	})
+}
+
+$http.afterRequest = function(){
+	uni.hideLoading();
+}
+
+// 封装弹窗展示消息提示的方法
+uni.$showMsg = function(title = '数据加载失败!',duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
+ 
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
